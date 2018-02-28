4e35093b201448c4d566f9b60e75fada050e7206
nguyen binh <nguyenngocbinh1412045@gmail.com>
2018-02-28 18:41:01 +0700

    QkTeam

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app=express();
+app.set("view engine","ejs");
+app.set("views","./views");
+app.listen(process.env.PORT || 8080);
+
+app.get("/",function(req,res){
+	res.render("trangchu");
+});
